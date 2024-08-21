15b17497ed48dbd8e73162bb2c86f5be0033c053
Bruno Shizukuda <shizukudabruno@gmail.com>
2024-08-21 12:05:18 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,45 +1,37 @@
-document.body.addEventListener("keyup", (event) => {
-    playSound((event.code.toLocaleLowerCase()))
-})
-
-document.querySelector('.composer button').addEventListener("click", () => {
-    let song = document.querySelector("#input").value
-
+document.body.addEventListener('keyup', (event) => {
+    playSound(event.code.toLowerCase());
+});
+document.querySelector('.composer button').addEventListener('click', () => {
+    let song = document.querySelector('#input').value;
     if(song !== '') {
-        let songArray = song.split('')
-        playComposition(songArray)
+        let songArray = song.split('');
+        playComposition(songArray);
     }
-})
+});
 
-
-function playSound(sound) {
-    let audioElement = document.querySelector(`#s_${sound}`)
-    let keyElement = document.querySelector(`div[data-key= "${sound}"]`)
-
-
+function playSound (sound){
+    let audioElement = document.querySelector(`#s_${sound}`);
+    let keyElement = document.querySelector(`div[data-key="${sound}"]`);
     if(audioElement) {
-        audioElement.currentTime = 0
-        audioElement.play()
+        audioElement.currentTime = 0;
+        audioElement.play();
     }
-
     if(keyElement) {
-        keyElement.classList.toggle('active')
-        
-        setTimeout(() => {
-            keyElement.classList.remove('active')
-        }, 200)
+        keyElement.classList.add('active');
     }
+    setTimeout(()=>{
+        keyElement.classList.remove('active');
+    }, 300);
 }
 
 function playComposition(songArray) {
-    let wait = 0
+    let wait = 0;
 
     for(let songItem of songArray) {
-        setTimeout(() => {
-            playSound(`key ${songItem}`)
+        setTimeout(()=>{
+            playSound(`key${songItem}`);
         }, wait);
 
-        wait += 250
-        
+        wait += 250;
     }
 }
